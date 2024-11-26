fb7709ed40461d6f31f372b8bf7d9ea4dd3d34f2
Joseph Young <musicjoeyoung@gmail.com>
2024-11-26 11:47:19 -0500

    added cors

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
-import 'dotenv/config';
+import "dotenv/config";
+
+import cors from "cors";
 import express from "express";
-import warehouseRoutes from "./routes/warehouseRoute.js";
 import inventoryRoutes from "./routes/inventoryRoute.js";
+import warehouseRoutes from "./routes/warehouseRoute.js";
 
 const app = express();
 const PORT = process.env.PORT || 5050;
 
 app.use(express.json());
+app.use(cors());
 
 app.get("/", (_req, res) => {
   res.send("Welcome to my API");
